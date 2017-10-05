d4f5aa166495ebbb2058a840b099fe1f41e0524d
Timo <timo@overmark.org>
2017-10-05 14:30:26 +0200

    New sounds, fixed input names, kill switch signal

--- synthesizer-lights/synthesizer-lights.ino
+++ synthesizer-lights/synthesizer-lights.ino
@@ -1,115 +1,124 @@
-#include <MozziGuts.h>
+
+/* 
+ *  DH2400 Physical Interaction and Design
+ *  Fall 2017
+ *  Group 11
+ *  Synthesizer and LED ring controller
+ *  
+ *  Inputs: A0 - A4: sensor data from another arduino
+ *  
+ */
 #import <Adafruit_NeoPixel.h>
+#include <MozziGuts.h>
 #include <Oscil.h>
+#include <Sample.h>
 #include <tables/chum9_int8.h> // recorded audio wavetable
 //#include <tables/triangle_valve_2048_int8.h>
-#include <tables/SIN4096_int8.h>
-#include <tables/whitenoise8192_int8.h> // recorded audio wavetable
-//#include <tables/SIN4096_int8.h>
-#include <tables/envelop2048_uint8.h>
+#include <tables/SIN8192_int8.h>
+//#include <tables/envelop2048_uint8.h>
 #include <LowPassFilter.h>
 #include <mozzi_rand.h>
 //#include <mozzi_midi.h>
 #include <RollingAverage.h>
 #include <ControlDelay.h>
+#include "Airsynt.h"
+#include "bass.h"
 
 #define CONTROL_RATE 64 // powers of 2 please
+//#define AUDIO_RATE 16384 // powers of 2 please
+
+#define INPUT_1 1
+#define INPUT_2 2
+#define INPUT_3 3
+#define INPUT_4 4
 
 //Initializing the LED ring
 Adafruit_NeoPixel ledstrip = Adafruit_NeoPixel(16, 7, NEO_GRB + NEO_KHZ800);
 
 //Oscillators for audio
-Oscil<CHUM9_NUM_CELLS, AUDIO_RATE> a1(CHUM9_DATA);
-Oscil<SIN4096_NUM_CELLS, AUDIO_RATE> a2(SIN4096_DATA);
+//sound 1
+Sample<AIRSYNTH_8192_NUM_CELLS, AUDIO_RATE> a1(AIRSYNTH_8192_DATA);
 
-// control oscillator for tremelo
-Oscil<SIN4096_NUM_CELLS, AUDIO_RATE> kVibrato(SIN4096_DATA);
-Oscil <SIN4096_NUM_CELLS, AUDIO_RATE> a3Sin0(SIN4096_DATA);
-Oscil <SIN4096_NUM_CELLS, AUDIO_RATE> a3Sin1(SIN4096_DATA);
-Oscil <SIN4096_NUM_CELLS, AUDIO_RATE> a3Sin2(SIN4096_DATA);
-Oscil <SIN4096_NUM_CELLS, AUDIO_RATE> a3Sin3(SIN4096_DATA);
+//sound 2
+Oscil<SIN8192_NUM_CELLS, AUDIO_RATE> a2(SIN8192_DATA);
+Oscil<SIN8192_NUM_CELLS, AUDIO_RATE> kVibrato(SIN8192_DATA);
 
+// sound 3
+Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> a3Sin0(SIN8192_DATA);
+Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> a3Sin1(SIN8192_DATA);
+Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> a3Sin2(SIN8192_DATA);
+Oscil <SIN8192_NUM_CELLS, AUDIO_RATE> a3Sin3(SIN8192_DATA);
 unsigned int echo_cells_1 = 32;
 unsigned int echo_cells_2 = 60;
 unsigned int echo_cells_3 = 127;
 ControlDelay <128, int> kDelay; // 2seconds
 
-//Sound 5
-Oscil<WHITENOISE8192_NUM_CELLS, AUDIO_RATE> a5(WHITENOISE8192_DATA);
-Oscil <SIN4096_NUM_CELLS, CONTROL_RATE> a5Mod(SIN4096_DATA);
-Oscil<SIN4096_NUM_CELLS, AUDIO_RATE> a5Howl(SIN4096_DATA);
-int a5HowlFreq = 440;
-int a5HowlDir = 3;
+//sound 4
+Sample<BASS_8192_NUM_CELLS, AUDIO_RATE> a4(BASS_8192_DATA);
 
 //Averaging for smoothing out input signals
 RollingAverage <int,16> input1;
 RollingAverage <int,16> input2;
 RollingAverage <int,16> input3;
 RollingAverage <int,16> input4;
-RollingAverage <int,16> input5;
 
 LowPassFilter lpf;
 
-int a1Intensity, a2Intensity,  a4Intensity, a5Intensity = 0;
+uint16_t ledN = 0; //led number to update. Only 1 led per control cycle
+
+boolean enableOutput = false;
+
+int a1Intensity, a2Intensity,  a3Intensity, a4Intensity = 0;
 
 void setup(){
+  pinMode(3,INPUT); //set pin 3 to work as a kill switch for output
+  
   startMozzi(CONTROL_RATE);
-  a1.setFreq(2.f); //sound 1
+  a1.setFreq(1.6f); //sound 1
+  a1.setLoopingOn();
   lpf.setResonance(200); //sound 1 filter
 
   a2.setFreq(440);
-  kVibrato.setFreq(.05f);
+  kVibrato.setFreq(10);
   kDelay.set(echo_cells_1);
 
-
-  a5.setFreq(1.f);
-  a5Mod.setFreq(.1f);
-  a5Howl.setFreq(420);
+  a4.setFreq(4.f);
+  a4.setLoopingOn();
 
   ledstrip.begin();
   ledstrip.show();
-  
 }
 
 void loop(){
   audioHook();
 }
 
-uint16_t ledN = 0;
 
 void updateControl(){
-  int value1 = input1.next(mozziAnalogRead(0));
-  int value2 = input2.next(mozziAnalogRead(1));
-  int value3 = input3.next(mozziAnalogRead(2));
-  int value4 = input4.next(mozziAnalogRead(3));
-  int value5 = input5.next(mozziAnalogRead(4));
-
-  //debug
-  value1 = 0;
+  enableOutput = (boolean) digitalRead(3);
   
-  // map the modulation into the filter range (0-255), not at full
-  lpf.setCutoffFreq(map(value1,0,1023,10,180));
+  int value1 = input1.next(mozziAnalogRead(INPUT_1));
+  int value2 = input2.next(mozziAnalogRead(INPUT_2));
+  int value3 = input3.next(mozziAnalogRead(INPUT_3));
+  int value4 = input4.next(mozziAnalogRead(INPUT_4));
 
-  a1Intensity = map(value3,0,1023,0,255);
+  a1Intensity = map(value1,0,1023,0,255);
+  //a1Intensity = 200;
+  
   a2Intensity = map(value2,0,1023,0,255);
-  //kVibrato.setFreq((float) 31+(value3>>4));
+  kVibrato.setFreq(a2Intensity >> 4);
   
-  a3Sin0.setFreq(value2);
-  a3Sin1.setFreq(kDelay.next(value2));
+  a3Sin0.setFreq(value3);
+  a3Sin1.setFreq(kDelay.next(value3));
   a3Sin2.setFreq(kDelay.read(echo_cells_2));
   a3Sin3.setFreq(kDelay.read(echo_cells_3));
 
   a4Intensity = map(value4,0,1023,0,255);
-  a5Intensity = map(value2,0,1023,0,255);
+  // map the modulation into the filter range (0-255), not at full
+  lpf.setCutoffFreq(map(value4,0,1023,10,230));
 
-  //debug:
-  //a1Intensity = a2Intensity =  a4Intensity = 0;
 
-  //a5Intensity = 128;
-  //a5Intensity = 128+a5Mod.next();
-  a5Howl.setFreq(a5HowlFreq+((int) a5Intensity));
-
-  Serial.println(a5Intensity);
+  
   /* LED ring color mapping:
      R: input1
      Y: input2
@@ -121,35 +130,48 @@ void updateControl(){
      => G = (i3 + (i2/4 + i4/4)) / 2
      => B = (i5 + (i4/2) /2
      */
-  uint32_t c = ledstrip.Color(
-    a1Intensity, a2Intensity, a4Intensity
-    //(value1>>2+(value2>>3)>>1),
-    //(value3>>2+(value2>>4+value4>>4) >>1),
-    //(value5>>2+(value4 >>3) >>1)
-  );
-  ledstrip.setPixelColor(ledN++, c);
-  ledstrip.show();
-  if(ledN>=ledstrip.numPixels()) ledN=0;
+  if(enableOutput) {
+    //idle mode
+    if(value1 < 40 && value2 < 40 &&  value3 < 40 && value4 < 40) {
+      uint32_t c = ledstrip.Color(64,64,64);
+      ledstrip.setPixelColor(ledN++, c);
+      ledstrip.show();
+      if(ledN>=ledstrip.numPixels()) ledN=0;
+    }
+    //normal mode
+    else {
+      uint32_t c = ledstrip.Color(
+        a1Intensity, a2Intensity, a4Intensity
+      );
+      ledstrip.setPixelColor(ledN++, c);
+      ledstrip.show();
+      if(ledN>=ledstrip.numPixels()) ledN=0;
+    }
+  }
+  else {
+      ledstrip.setPixelColor(ledN++, ledstrip.Color(0,0,0));
+      ledstrip.show();
+  }
 }
 
+
+
 int updateAudio(){
-  //Q15n16 vibrato = (Q15n16) a2Intensity * kVibrato.next();
-  //int a4audio = a4Carrier.phMod((int)a4Modulator.next()*(150u+a4ModWidth.next()));
-  //a4audio *= (byte)a4Envelop.next()*a4Intensity;
-  //a4audio >>= 8;
-  return (int) (
-    
-    (lpf.next(a1.next()) * a1Intensity >>8)+
-    //(a2Intensity*a2.phMod(vibrato) >>8)
-    (3*((int)a3Sin0.next()+a3Sin1.next()+(a3Sin2.next()>>1)+(a3Sin3.next()>>2) >>3)) +
-    //a4audio + 
-    (a5.next()*a5Intensity >>8 ) +
-    (a5Howl.next()*a5Intensity >>8 )
-   // >> 2
-    //) ;
-   //return a2Intensity*a2.next() >>8;
-    //return a2.next();
+  Q15n16 vibrato = (Q15n16) kVibrato.next() * a2Intensity;
+ 
+  if(enableOutput) return (int) (
+    (long)
+    (a1.next() * a1Intensity >>8)
+    +
+    (a2.phMod(vibrato)*a2Intensity >>8 )
+    +
+    (3*((int)a3Sin0.next()+a3Sin1.next()+(a3Sin2.next()>>1)+(a3Sin3.next()>>2) >>3))
+    +
+    (lpf.next(a4.next())*a4Intensity >>8 )
+
+    >> 2
     );
+  else return 0;
 }
 
 
